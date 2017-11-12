0946b3b121b3bc72a0f63a77fc4182ba20f35b2e
Santosh T <bt.santosh01@gmail.com>
2017-11-12 18:01:28 +0530

    added pagination to ajax call for search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,24 @@
 class SearchController < ApplicationController
+	require 'will_paginate/array'
+
 	def show
 	end
 	
 	
 	def search_content
 		@results = []
+		@total_results = 0
 		@query = params[:search]		
-		@results = Article.search(params[:search], :match_mode => :any)  unless params[:search].blank?
+		unless params[:search].blank?
+			results = Article.search(params[:search], :match_mode => :any)  
+			@total_results = Article.search_count(params[:search], :match_mode => :any)
+			@results = results.paginate(page: params[:page], per_page: 5)
+		end		
+
+		respond_to do |format|
+		    format.html 
+		    format.json { render json: @results }
+		    format.js
+		end
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,7 @@ Rails.application.routes.draw do
   end
 
   get 'search', to: 'search#show'
-  post 'search/articles', to: 'search#search_content'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'search/articles', to: 'search#search_content'
 
   root to: 'home#welcome'
 end
